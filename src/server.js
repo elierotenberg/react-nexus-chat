9a031180619883a25f5c96832c33c618cdf6bbfa
Elie Rotenberg <elie@rotenberg.io>
2014-11-22 16:43:33 +0100

    wc

--- src/server.js
+++ src/server.js
@@ -8,17 +8,22 @@ const path = require('path');
 const common = require('./common');
 const App = require('./App');
 
+function listening(name, port) {
+  return () => console.log(`${name} listening on port ${port}.`);
+}
+
 // Render + static server
 express()
 .use(cors())
 .use(express.static(path.join(__dirname, '..', 'public')))
 .get('/favicon.ico', (req, res) => res.status(404).send(null))
 .use((new App()).prerender)
-.listen(common.render.port);
+.listen(common.render.port, listening('render', common.render.port));
 
 (new UplinkSimpleServer({ guid: _.guid(),
   stores: [],
   rooms: [],
   actions: [],
   app: express().use(cors()),
-})).listen(common.uplink.port);
+}))
+.listen(common.uplink.port, listening('uplink', common.uplink.port));
